49694e299c1e9094c180ec01e30f538b4f6bc09d
tomasvtx <tomasvytiska@seznam.cz>
2023-12-21 22:43:15 +0100

    Přidejte soubory projektu.

---
+++ AppLauncher/AppLauncher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace AppLauncher
+    {
+        public class AppLauncher : ILauncher
+        {
+            public int LaunchAction(
+                string filePath ,
+                bool runAsAdministrator ,out string error,
+                bool waitForExit = true ,
+                string arguments = null ,
+                string workingDirectory = null)
+            {
+                error = String.Empty;
+
+            if (!File.Exists(filePath))
+                {
+                    error = "Application not found.";
+                    return -1; // Indicate an error with a negative exit code
+                }
+
+                var exitCode = -1; // Default exit code for failure
+
+                    var installerProcess = new Process()
+                    {
+                        StartInfo = new ProcessStartInfo(filePath)
+                        {
+                            UseShellExecute = true,
+                            Verb = runAsAdministrator ? "runas" : "",
+                            Arguments = arguments,
+                            WorkingDirectory = workingDirectory
+                        }
+                    };
+
+                    try
+                    {
+                        installerProcess.Start( );
+
+                        if (waitForExit)
+                        {
+                            installerProcess.WaitForExit( );
+                            exitCode = installerProcess.ExitCode; // Retrieve the exit code
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        error = $"Error running application: {ex.Message}";
+                    }
+
+                return exitCode;
+            }
+        }
+    }
---
+++ AppLauncher/ILauncher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLauncher
+{
+    public interface ILauncher
+    {
+        int LaunchAction(
+            string filePath ,
+            bool runAsAdministrator , out string error ,
+            bool waitForExit = true ,
+            string arguments = null ,
+            string workingDirectory = null);
+    }
+    }
